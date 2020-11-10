f718a4b91cabb6339408251ea65111231a2721a2
daru365 <70891795+daru365@users.noreply.github.com>
2020-11-10 13:20:44 +0300

    Защита проекта

--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,9 @@
 const modalLink = document.querySelector(".button-contacts");
 const modalPopup = document.querySelector(".modal-contacts");
 const modalClose = modalPopup.querySelector(".modal-close");
+const modalName = modalPopup.querySelector(".name-user");
+const modalLogin = modalPopup.querySelector(".login-user");
+const modalButton = modalPopup.querySelector(".modal-button");
 
 modalLink.addEventListener("click", function (evt) {
   evt.preventDefault();
@@ -19,3 +22,27 @@ window.addEventListener("keydown", function (evt) {
       mapPopup.classList.remove("modal-on");
     }
   }
+});
+
+modalPopup.addEventListener("submit", function (evt) {
+	
+	if (!modalName.value || !modalLogin.value) {
+		evt.preventDefault();
+		 if (!modalName.value) {
+		 	modalName.classList.add("modal-input-error")
+		 } else {
+		 	modalName.classList.remove("modal-input-error")
+		 }
+
+		  if (!modalLogin.value) {
+		 	modalLogin.classList.add("modal-input-error")
+		 } else {
+		 	modalLogin.classList.remove("modal-input-error")
+		 }
+
+		modalPopup.classList.remove("modal-error");
+		modalPopup.offsetWidth = modalPopup.offsetWidth;
+		modalPopup.classList.remove("modal-on");
+		modalPopup.classList.add("modal-error");
+	}
+});
